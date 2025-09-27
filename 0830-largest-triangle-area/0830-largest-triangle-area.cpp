97c68df2790381a38e86f092e70f5b5f1d54e664
Srushti Ahire <158837536+CodeSrushti-3105@users.noreply.github.com>
2025-09-27 17:24:02 +0530

    Time: 0 ms (100%), Space: 10.6 MB (34.38%) - LeetHub

--- 0830-largest-triangle-area/0830-largest-triangle-area.cpp
+++ 0830-largest-triangle-area/0830-largest-triangle-area.cpp
@@ -4,20 +4,19 @@ public:
         double maxArea = 0.0;
         int n = points.size();
 
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                for(int k = j + 1; k < n; k++) {
-                    // Shoelace formula
+        for(int i = 0;i < n; i++){
+            for(int j = i+1; j < n;j++){
+                for(int k = j+1; k < n; k++){
                     double area = abs(
                         points[i][0] * (points[j][1] - points[k][1]) +
                         points[j][0] * (points[k][1] - points[i][1]) +
-                        points[k][0] * (points[i][1] - points[j][1])
-                    ) / 2.0;
+                        points[k][0] * (points[i][1] - points[j][1]) 
+                    )/2.0;
+
                     if(area > maxArea) maxArea = area;
                 }
             }
         }
-
         return maxArea;
     }
 };
